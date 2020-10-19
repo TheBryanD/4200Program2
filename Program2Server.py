eeafa273799f08283ccb4acc6b88d72354adad7b
Bryan <TheBryanD@users.noreply.github.com>
2020-10-19 17:13:22 -0500

    Started on Creating Packets to send back

--- Program2Server.py
+++ Program2Server.py
@@ -1,4 +1,5 @@
 import socket
+from socket import RCVALL_IPLEVEL
 import struct
 import sys
 import time
@@ -7,13 +8,13 @@ from typing import Sequence
 
 
 #creates packets of data to send
-def createPacket(sequence_nunmber, ack_number, ack, syn, fin, payload):
-    data = struct.pack('!I', sequence_nunmber)
-    data += struct.pack('!I', ack_number)
+def createPacket(sequence_nunmber, ack_number, ack, syn, fin):
+    data = struct.pack('32s', sequence_nunmber)
+    data += struct.pack('32s', ack_number)
+    data += struct.pack('29s', '')
     data += struct.pack("!c", ack)
     data += struct.pack("!c", syn)
     data += struct.pack("!c", fin)
-    data += struct.pack('512s', payload)
     return data
 
 
@@ -33,10 +34,14 @@ except:
     sys.exit(0)
 
 #step 2 - save webpage
-response = urllib.request.urlopen(webpageToDownload)
-webcontent = response.read()
-localWebFileSave = open('webPage.html', 'wb').write(webcontent)
-localWebFileSave.close()
+opener = urllib.request.FancyURLopener({})
+f = opener.open(webpageToDownload)
+content = f.read()
+localWebFileSave = open('C:\\Sample Files\\webpage.html', 'w').write(str(content))
+#response = urllib.request.urlopen(webpageToDownload)
+#webcontent = response.read()
+#localWebFileSave = open('webPage.html', 'wb').write(webcontent)
+#localWebFileSave.close()
 print(str(webpageToDownload) + " was saved to local storage.")
 
 #step 3 - specify where the server should listen on, IP and port
@@ -46,20 +51,49 @@ print("Address : ", server_addr_obj)
 print("Port : ", port)
 
 while True:
-    #step 4 - create data to send
     try:
-        data = ''
-        send_data = createPacket(100, 0, 'Y', 'N', 'N', data)
+    #Receive data from client
+        recvData, addr = sock.recvfrom(1024)
+        unpacker = struct.Struct('32s32s29sccc')
+        unpacker.unpack(recvData)
 
-    #step 5 - send data to client
-        data, addr = sock.recvfrom(1024)
-        sock.sendto(addr, send_data)
-        unpacker = struct.Struct('iii8s')
+        #send data back to client and log
+        if int(recvData[0]) == 12345:
+            #send ack handshake packet
+            if recvData[4] == 'Y':
+                syn = 'Y'
+            else: 
+                syn = 'N'
+            sqnc_num = int(recvData[0])
+            header = createPacket(100, sqnc_num+1, 'Y', syn, 'N')
+            sock.sendto(addr, header)
 
-        if(data):
-            print(data)
+            file.write("RECV ", sqnc_num, " ", recvData[1], " ", recvData[3], " ", recvData[4], " ", recvData[5])
+            file.write("SEND ", 100, " ", sqnc_num+1, " ", 'Y', " ", syn, " ", 'N')
+        else:
+            isLastPacket = False
+            #create packet to send back
+            sqnc_num = recvData[1] +1
+            ack_num = recvData[0] +1
+            ack = 'Y'
+            syn = 'N'
+            if isLastPacket == True:
+                fin = 'Y'
+            else:
+                fin = 'N'
+            header = createPacket(sqnc_num, ack_num, ack, syn, fin)
+            sock.sendto(header, addr)
+
+            file.write("RECV ", sqnc_num, " ", recvData[1], " ", recvData[3], " ", recvData[4], " ", recvData[5])
+            file.write("SEND ", ack_num, " ", sqnc_num+1, " ", ack, " ", syn, " ", fin)
+
+        #print data to screen
+        if(recvData):
+            print(recvData)
         else:
             time.sleep(2)
+
+
     except KeyboardInterrupt: #CTRL+^C
         sock.close()
         file.close()
