c77606b46df2fa2d3765005329b37773baaac74f
DontSpookyMe <72959907+DontSpookyMe@users.noreply.github.com>
2020-10-19 19:57:49 -0400

    Add files via upload

--- Program2Client.py
+++ Program2Client.py
@@ -2,17 +2,16 @@ import socket
 import struct
 import sys
 import time
-from collections import namedtuple
 
 #creates packets of data to send
-def createPacket(sequence_number, ack_number, ack, syn, fin, payload):
-    data = struct.pack('!I', sequence_number)
-    data += struct.pack('!I', ack_number)
-    data += struct.pack("!c", ack)
-    data += struct.pack("!c", syn)
-    data += struct.pack("!c", fin)
-    data += struct.pack('512s', payload)
-    return data
+def createPacket(sequence_number, ack_number, ack, syn, fin):
+	data = struct.pack('!32s', sequence_number.encode())
+	data += struct.pack('!32s', ack_number.encode())
+	data += struct.pack('29s', ''.encode())
+	data += struct.pack("!c", ack.encode())
+	data += struct.pack("!c", syn.encode())
+	data += struct.pack("!c", fin.encode())
+	return data
 
 #step 1 - create the socket object
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -33,16 +32,24 @@ server_addr = (ip, port)
 print("Server address: ", server_addr)
 print("Server port: ", port)
 
-def send(seq_num, ack_num, ack, syn, fin, data, addr):
+def send(seq_num, ack_num, ack, syn, fin, addr):
 	if(addr[0] != server_addr[0] and addr[1] != server_addr[1]):
 		sys.exit(1)
 
-	send_data = createPacket(seq_num, ack_num, ack, syn, fin, data)
-	sock.sendto(send_data, server_addr)
+	send_data = createPacket(seq_num, ack_num, ack, syn, fin)
+	sock.sendto(send_data, addr)
+	print("I'm here")
 	
 def recv():
 	data, addr = sock.recvfrom(1024)
 	seq_num, ack_num, notUsed, ack, syn, fin = struct.unpack('32s32s29sccc', data)
+	seq_num = seq_num.decode()
+	ack_num = ack_num.decode()
+	notUsed = notUsed.decode()
+	ack = ack.decode()
+	syn = syn.decode()
+	fin = fin.decode()
+	
 	if(data):
 		print(data)
 	else:
@@ -58,8 +65,9 @@ data = ''
 
 while True:
 	try:
+
 		if(seq_num == 12345):
-			send(seq_num, ack_num, ack, syn, fin, data, server_addr)
+			send(seq_num, ack_num, ack, syn, fin, server_addr)
 			file.write("SEND ", seq_num, " ", ack_num, " ", ack, " ", syn, " ", fin)
 
 			seq_num, ack_num, notUsed, ack, syn, fin, addr = recv()
@@ -67,7 +75,7 @@ while True:
 
 			ack_num += 1
 			syn = 'N'
-			send(seq_num, ack_num, ack, syn, fin, data, addr)
+			send(seq_num, ack_num, ack, syn, fin, addr)
 			file.write("SEND ", seq_num, " ", ack_num, " ", ack, " ", syn, " ", fin)
 		else:
 			seq_num, ack_num, notUsed, ack, syn, fin, addr = recv()
@@ -76,12 +84,12 @@ while True:
 			if(fin == 'Y'):
 				ack = 'Y'
 				ack_num += 512
-				send(seq_num, ack_num, ack, syn, fin, data, addr)
+				send(seq_num, ack_num, ack, syn, fin, addr)
 				file.write("SEND ", seq_num, " ", ack_num, " ", ack, " ", syn, " ", fin)
 				exit()
 
 			ack_num += 512
-			send(seq_num, ack_num, ack, syn, fin, data, addr)
+			send(seq_num, ack_num, ack, syn, fin, addr)
 			file.write("SEND ", seq_num, " ", ack_num, " ", ack, " ", syn, " ", fin)
 
 	except KeyboardInterrupt:
